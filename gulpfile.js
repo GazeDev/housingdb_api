f4be7f39bde60757b0fdb087707cf0c1e5f2ff51
zaclittleberry <zlittleberry@linuxfoundation.org>
2019-06-08 02:28:23 -0400

    get gulp running again

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,14 @@ gulp.task('test', function () {
   let argArray = [
     '--verbose',
     '--assert',
-    'code',
+    '@hapi/code',
   ];
   // build the files to test
   for (let mod of modules) {
     argArray.push(`lib/${mod}/${mod}.tests.js`);
   }
-  return gulp.src('test')
+  return gulp.src('test', { allowEmpty: true })
     .pipe(lab(argArray));
 });
 
-gulp.task('default', ['test']);
+gulp.task('default', gulp.parallel('test'));
