d6a0d43808e1fa5e1ca88cdeac52db26181f32b0
zaclittleberry <zlittleberry@linuxfoundation.org>
2019-07-25 14:56:29 -0400

    reformat to look more like boilerplate. console error instead of throw

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = (async() => {
 
   for (let envVar of envVars) {
     if (!process.env[envVar]) {
-      throw `Error: Make sure you have ${envVar} in your environment variables.`;
+      console.error(`Error: Make sure you have ${envVar} in your environment variables.`);
     }
   }
 
